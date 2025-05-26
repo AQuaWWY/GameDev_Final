58990bbe857b89bd48689f8bcc0123f7237fecb6
tearxiaorain <1477718542@qq.com>
2025-05-26 18:13:34 +0800

    在角色前方随机生成道具，超出距离销毁

---
+++ Assets/scripts/item.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class item : MonoBehaviour
+{
+    GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GameObject.Find("Temp_man");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < obj.transform.position.z - 4f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/picker.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal.Profiling.Memory.Experimental;
+using UnityEngine;
+
+public class picker : MonoBehaviour
+{
+    public float range_x = 15f;
+    public float range_z = 1f;
+    public GameObject[] prefabs;
+    GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GameObject.Find("Temp_man");
+        Invoke("SpawnItem", 0.1f);
+    }
+
+    void SpawnItem()
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            // 随机选择一个预制体
+            GameObject prefab = prefabs[Random.Range(0, prefabs.Length)];
+
+            // 随机生成位置
+            Vector3 randomPosition = new Vector3(
+            Random.Range(-range_x / 2, range_x / 2),
+            0,
+            Random.Range(-range_z / 2 + obj.transform.position.z + 30f, range_z / 2 + obj.transform.position.z + 30f)
+            );
+
+            // 实例化物体
+            Instantiate(prefab, randomPosition, Quaternion.identity);
+        }
+        Invoke("SpawnItem", 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
