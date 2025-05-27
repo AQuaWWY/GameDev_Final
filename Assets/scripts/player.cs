9c0f992a77842d8a0871061b5b9fbc1822af06ed
tearxiaorain <1477718542@qq.com>
2025-05-27 11:05:13 +0800

    检测碰撞对象，尚有问题
    无法识别地刺的tag，沿途发生莫名其妙的碰撞

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
@@ -8,19 +9,54 @@ public class NewBehaviourScript : MonoBehaviour
     public float speed_forword = 2f; // 控制向前移动速度
     public Transform m_transform;
     public float edge = 5f;
+    public int current_score = 0; //分数
+    public int current_health = 3; //生命值
+    GameObject rest;
+    GameObject score;
+    TextMeshProUGUI rest_text;
+    TextMeshProUGUI score_text;
 
     // 初始化
     void Start()
     {
         m_transform = this.transform;
+        rest = GameObject.Find("health");
+        score = GameObject.Find("score");
+        rest_text = rest.GetComponent<TextMeshProUGUI>();
+        score_text = score.GetComponent<TextMeshProUGUI>();
+        current_score = 0;
+        current_health = 3;
     }
     void OnTriggerEnter(Collider coll)
     {
-        if(coll.gameObject.tag == "Item")
+        Debug.Log("发生碰撞");
+        if(coll.gameObject.tag == "Item") //得分
         {
+            current_score++;
+            score_text.text = "current score: " + current_score.ToString();
             GameObject collidedWith = coll.gameObject;
             // 销毁碰撞到的物体
             Destroy(collidedWith);
+        }
+        else if(coll.gameObject.tag == "Heal") //回血
+        {
+            current_health++;
+            rest_text.text = "rest: " + current_health.ToString();
+            GameObject collidedWith = coll.gameObject;
+            // 销毁碰撞到的物体
+            Destroy(collidedWith);
+        }
+        else if(coll.gameObject.tag == "damage")
+        {
+            Debug.Log("碰到障碍物");
+            current_health--;
+            rest_text.text = "rest: " + current_health.ToString();
+        }
+        else
+        {
+            Debug.Log("默认为障碍物");
+            current_health--;
+            rest_text.text = "rest: " + current_health.ToString();
         }
     }
 
